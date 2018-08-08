54f1912b5bbd7d67d4e7e1bd554c759a66bfe2ba
Josiah Nunemaker <yoshiya.d.n@gmail.com>
2018-08-08 12:36:33 -0400

    Remove unnecessary database checks

--- server/src/db.js
+++ server/src/db.js
@@ -1,7 +1,6 @@
 import knex from 'knex';
 import config from './config';
 
-process.env.SQL_INSTANCE_CONNECTION_NAME &&
 process.env.NODE_ENV === 'production'
   ? console.log('using prod')
   : console.log('using dev');
@@ -9,7 +8,6 @@ process.env.NODE_ENV === 'production'
 const db = knex({
   client: 'mysql',
   connection:
-    process.env.SQL_INSTANCE_CONNECTION_NAME &&
     process.env.NODE_ENV === 'production'
       ? config.production.db
       : config.dev.db,
